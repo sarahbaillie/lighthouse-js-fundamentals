85a1051392e76f97bf2a75cdf5dde0f909329b78
Sarah Anne Baillie <sarahannebaillie@gmail.com>
2020-07-15 17:16:46 +0000

    looping ranges exercise

---
+++ looping-rangs.js
@@ -0,0 +1,17 @@
+function range (start, end, step) {
+  var array = [];
+  if (start === undefined || end === undefined || step === undefined || start > end || step < 0) {
+    array = [];
+  } else {
+    let counter = start;
+    while (counter <= end) {
+      array.push(counter);
+      counter += step;
+    }
+  }
+  return array;
+}
+		
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
